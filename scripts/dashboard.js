490ede3109fcff2c59c1748f0d6433c32e3d214a
EzekielBliss <olatunjiezekiel27@gmail.com>
2025-09-12 07:04:09 +0100

    add

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -16,20 +16,20 @@ if (!firebase.apps.length) {
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-const userId = firebase.auth().currentUser.uid;
+// const userId = firebase.auth().currentUser.uid;
 
-db.collection("transactions")
-  .where("userId", "==", userId)
-  .orderBy("timestamp", "desc")
-  .onSnapshot((querySnapshot) => {
-    const transactions = [];
-    querySnapshot.forEach((doc) => {
-      transactions.push({ id: doc.id, ...doc.data() });
-    });
+// db.collection("transactions")
+//   .where("userId", "==", userId)
+//   .orderBy("timestamp", "desc")
+//   .onSnapshot((querySnapshot) => {
+//     const transactions = [];
+//     querySnapshot.forEach((doc) => {
+//       transactions.push({ id: doc.id, ...doc.data() });
+//     });
 
-    // Now update your dashboard UI with this data
-    displayTransactions(transactions);
-  });
+//     // Now update your dashboard UI with this data
+//     displayTransactions(transactions);
+//   });
 
 
 // Format currency
